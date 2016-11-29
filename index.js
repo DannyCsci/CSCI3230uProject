9d927f436161cfded62f0291d2034a22bcd4f408
Dai Chan <vincent.dy.chan@gmail.com>
2016-11-28 20:09:19 -0500

    Registration appends to database. Login not functional.  Added some other files.

--- index.js
+++ index.js
@@ -9,15 +9,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
-var User = mongoose.model('User', new Schema({
-    id: ObjectId,
-    username: String,
-    password: String
-}));
-
 
 //Connect to Database of Login Details
 mongoose.connect('mongodb://localhost/userDB');
+
+// To represent a user in database
+var User = mongoose.model('User', new Schema({
+    id: ObjectId,
+    firstName: String,
+    lastName: String,
+    userName: {type: String, unique: true },
+    password: String,
+}));
+
+
+
 
 // Middleware
 
@@ -27,13 +33,15 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // session (parses session IDs and loads data)
+
 app.use(session({
   genid: function(request) { return uuid.v4(); },
-  resave: false /* save only when changes */,
+  resave: false, /* save only when changes */
   saveUninitialized: false /* save only when data */,
   /*cookie: { secure: true; },*/
-  secret: 'apollo slackware propositional expectations'
+  secret: 'gentlemen incredible eh insurance'
 }));
+
 
 // Setting up views engine for PUG Template
 app.set('views', __dirname + '/views');
@@ -58,21 +66,6 @@ app.get('/register', function(request, response){
 
 });
 
-app.post('/register', function(request, response){
-    var user = new User({
-        firstName: request.body.firstName,
-        lastName: request.body.lastName,
-        userName: request.body.userName,
-        password: request.body.password
-    });
-    
-    user.save(function(error){
-        if (error) {
-            var errMessage = "Oops! Something went wrong! Try Again!";
-            
-        }
-    });
-});
 
 app.get('/about', function(request, response){
     
@@ -81,26 +74,77 @@ app.get('/about', function(request, response){
     
 });
 
-app.post('/loggingIn', function(request, response){
-    console.log('form submitted: ' + request.body);
-    var username = request.body['username'];
-    if (userExists(username)) {
-        //Store the username in the session
-        request.session.username = username;
-
-        // redirect to homepage
-        response.redirect('/');
-    }
-    else {
-        // Show login page again /w Error Message
-        response.render('login', {errorMessage: 'Login Incorrect'});
+app.get('/dashboard', function(request, response){
+    if (request.session && request.session.user) {
+        User.findOne({username: request.session.user.userName }, function(error, user) {
+           if (!user) {
+               request.session.destroy();
+               response.redirect('login');
+           } 
+            else {
+                response.locals.user = user;
+                response.render('dashboard');
+            }
+        });
+    }else{
+        response.redirect('login');   
     }
 });
+
+
 
 app.get('/logout', function(request, response){
-    request.session.username = '';
+    
+    request.session.destroy();
     response.redirect('/');
 });
+
+app.post('/processLogin', function(request, response){
+    //console.log('form submitted: ' + request.body);
+    
+    User.findOne({username: request.body.username}, function(err, user){
+        if (!user) {
+            response.render('login', {error: 'Invalid username or password'});
+        }
+        else if (request.body.password === user.password) {
+                request.session.user = user; 
+                response.redirect('/dashboard');
+            }
+        else {
+            response.render('login', {error: 'Invalid username or password.'});
+        }
+    });
+        
+});
+    
+
+app.post('/processRegistration', function(request, response){
+    var user = new User({
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        userName: request.body.userName,
+        password: request.body.password
+    });
+    
+    user.save(function(err){
+        if (err) {
+            var err = 'Oops! Something went wrong! Try Again!';
+            if (err.code === 11000) {
+                error = 'That username is already taken.';
+            }
+            response.render('/register', {error: error});
+            
+        }
+        else {
+            response.redirect('/dashboard');
+        }
+            response.redirect('/dashboard');
+    });
+});
+
+
+
+
 
 // express setup
 app.set('port', process.env.PORT || 3000);
